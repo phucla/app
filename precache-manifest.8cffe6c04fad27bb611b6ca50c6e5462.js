3852f9c82de79c54e2f009412c07578569cfa08a
Phuc La <phuc.la@asnet.com.vn>
2020-12-25 09:35:44 +0700

    Updates

---
+++ precache-manifest.8cffe6c04fad27bb611b6ca50c6e5462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d1e7b1f137984361b730cdeab0d141",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "2f624bb7f2e0df294e9d",
+    "url": "/app/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "2f624bb7f2e0df294e9d",
+    "url": "/app/static/js/2.a051f7da.chunk.js"
+  },
+  {
+    "revision": "966d139a5896f69b32b4c123a2d69c56",
+    "url": "/app/static/js/2.a051f7da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ebed8f8e7eb8754d5e2",
+    "url": "/app/static/js/main.3618e78e.chunk.js"
+  },
+  {
+    "revision": "ff829e0cce35ada8612b",
+    "url": "/app/static/js/runtime-main.87488013.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/time-tracking/precache-manifest.d7b236e508e3c81436a53a2b93897e08.js"
+  "/app/precache-manifest.8cffe6c04fad27bb611b6ca50c6e5462.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/time-tracking/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
