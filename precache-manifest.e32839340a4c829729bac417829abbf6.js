05cd43de744800c0b0fd1d5a0d1aafb84528b0a0
Phuc La <phuc.la@asnet.com.vn>
2021-01-23 23:24:39 +0700

    Updates

---
+++ precache-manifest.e32839340a4c829729bac417829abbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9dc4261ed174dc05fbd417bcc2f1bcd5",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "d70d9cdefb34c11b6012",
+    "url": "/app/static/css/2.18fb2ca0.chunk.css"
+  },
+  {
+    "revision": "d70d9cdefb34c11b6012",
+    "url": "/app/static/js/2.758291e4.chunk.js"
+  },
+  {
+    "revision": "46fe7e45b3f552be56a35568a9688b34",
+    "url": "/app/static/js/2.758291e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5bb66e962b18d74ef51",
+    "url": "/app/static/js/main.0e1d07ff.chunk.js"
+  },
+  {
+    "revision": "ff829e0cce35ada8612b",
+    "url": "/app/static/js/runtime-main.87488013.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.35edde922c73742103add5c4d02a5ccc.js"
+  "/app/precache-manifest.e32839340a4c829729bac417829abbf6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/app/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
